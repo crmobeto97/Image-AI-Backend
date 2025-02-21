a2ea9e78fd56f00c82bcfb295ba652956b7fb24e
ivanA10 <103442331+ivanA10@users.noreply.github.com>
2025-02-20 21:41:23 -0600

    image controller

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from starlette.responses import RedirectResponse
-
+from app.routes import images
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Python Quickstart using FastAPI",
@@ -17,7 +18,16 @@ app = FastAPI(
     #lifespan=lifespan,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las fuentes (puedes restringirlo)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
+
+app.include_router(images.router, prefix="/images", tags=["images"])
 
 # Redirect to Swagger documentation on loading the API for demo purposes
 @app.get("/", include_in_schema=False)
---
+++ src/app/routes/images.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, File, UploadFile, HTTPException, Query, Form
+import os
+from pathlib import Path
+from typing import List
+import uuid
+
+router = APIRouter()
+
+TEMP_DIR =  Path("/temp") 
+TEMP_DIR.mkdir(parents=True, exist_ok=True)  
+
+@router.get("/")
+async def check_folder(uuid: str = Query(..., title="UUID de la carpeta")):
+    try:
+        folder_path = TEMP_DIR / uuid  
+ 
+        if not folder_path.exists():
+            folder_path.mkdir(parents=True, exist_ok=True)
+            return {"message": "Carpeta creada", "files": []}
+
+        image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"}
+        image_files = [
+            file.name for file in folder_path.iterdir()
+            if file.is_file() and file.suffix.lower() in image_extensions
+        ]
+
+        return {"status": True, "message": "Carpeta encontrada", "data": image_files}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al verificar carpeta: {str(e)}")
+
+@router.post("/")
+async def upload_file(token: str = Form(...), files: List[UploadFile] = File(...)):
+    try:
+
+        folder_path = TEMP_DIR / token  
+
+        for file in files: 
+            file_extension = file.filename.split(".")[-1]
+            new_filename = f"{uuid.uuid4()}.{file_extension}"  # Generar un nuevo nombre único
+            file_location = folder_path / new_filename  # Ruta con el nuevo nombre
+            with open(file_location, "wb") as file_object:
+                file_object.write(await file.read()) 
+
+        return {"message": "Archivos guardados exitosamente"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al guardar archivo: {str(e)}")
