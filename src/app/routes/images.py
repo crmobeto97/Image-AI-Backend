e5cdf984011ed97d390b1ca69649f171abeca2d3
Beto Ocampo <crmobeto97@gmail.com>
2025-04-15 23:38:16 -0600

    add raw folder and process image

--- src/app/routes/images.py
+++ src/app/routes/images.py
@@ -7,6 +7,7 @@ import uuid
 router = APIRouter()
 
 TEMP_DIR =  Path("/temp") 
+RAW_FOLDER =  Path("raw") 
 TEMP_DIR.mkdir(parents=True, exist_ok=True)  
 
 @router.get("/")
@@ -38,6 +39,45 @@ async def upload_file(token: str = Form(...), files: List[UploadFile] = File(...
         for file in files: 
             file_extension = file.filename.split(".")[-1]
             new_filename = f"{uuid.uuid4()}.{file_extension}"  # Generar un nuevo nombre único
+            file_location = folder_path / new_filename  # Ruta con el nuevo nombre
+            with open(file_location, "wb") as file_object:
+                file_object.write(await file.read()) 
+
+        return {"message": "Archivos guardados exitosamente"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al guardar archivo: {str(e)}")
+
+@router.get("/raw")
+async def check_folder(uuid: str = Query(..., title="UUID de la carpeta")):
+    try:
+        folder_path = TEMP_DIR / uuid / RAW_FOLDER 
+ 
+        if not folder_path.exists():
+            folder_path.mkdir(parents=True, exist_ok=True)
+            return {"message": "Carpeta creada", "files": []}
+
+        image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"}
+        image_files = [
+            file.name for file in folder_path.iterdir()
+            if file.is_file() and file.suffix.lower() in image_extensions
+        ]
+
+        return {"status": True, "message": "Carpeta encontrada", "data": image_files}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al verificar carpeta: {str(e)}")
+
+@router.post("/raw")
+async def upload_file(token: str = Form(...), files: List[UploadFile] = File(...)):
+    try:
+
+        folder_path = TEMP_DIR / token / RAW_FOLDER 
+
+        for file in files: 
+            file_extension = file.filename.split(".")[-1]
+            new_filename = f"{uuid.uuid4()}.{file_extension}"  # Generar un nuevo nombre único
+            print(folder_path)
             file_location = folder_path / new_filename  # Ruta con el nuevo nombre
             with open(file_location, "wb") as file_object:
                 file_object.write(await file.read()) 
--- src/app/routes/model.py
+++ src/app/routes/model.py
@@ -5,31 +5,38 @@ from pathlib import Path
 router = APIRouter()
 # Path to the folder containing images
 TEMP_DIR =  Path("/temp") 
+RAW_FOLDER =  Path("raw") 
+PROCESSED_FOLDER =  Path("processed") 
 
 @router.post("/")
 def detect_objects(token: str = Form(...)):
     try:
-        folder_path = TEMP_DIR / token  
+        folder_path = TEMP_DIR / token / RAW_FOLDER
         results = []
-
+        processed_path = TEMP_DIR / token / PROCESSED_FOLDER
         # Ensure the temp directory exists
         if not folder_path.exists():
             print("Temp directory not found.")
             return {"error": "Temp directory not found."}
+        
+        
+        if not processed_path.exists():
+            processed_path.mkdir(parents=True, exist_ok=True)
+            print(f"Carpeta creada: {processed_path}")
 
         # Loop through images in the temp folder
         for image_path in folder_path.glob("*.jpg"):  # Adjust for other formats if needed
             print(f"Processing: {image_path}")
 
             # Command to run YOLO detection
-            cmd = [
+            cmd_red = [
                 "./darknet", "detect",
                 "cfg/yolov3.cfg", "yolov3.weights",
                 str(image_path)
             ]
 
             # Run the command
-            result = subprocess.run(cmd, cwd= Path("ia_model/darknet"),capture_output=True, text=True)
+            result = subprocess.run(cmd_red, cwd= Path("ia_model/darknet"),capture_output=True, text=True)
             print(result.stdout)
             # Store results
             results.append({
@@ -38,7 +45,25 @@ def detect_objects(token: str = Form(...)):
                 "stderr": result.stderr
             })
 
-            
+            image_name_without_ext = image_path.name.removesuffix(".jpg")
+            #print(image_name_without_ext)  
+            image_processed_name = image_name_without_ext + "-processed.jpg"
+            image_processed_path_name = str(processed_path) + "/" + image_processed_name 
+            print(image_processed_path_name)
+            cmd_move = [
+                "mv", "predictions.jpg",
+                str(image_processed_path_name)
+            ]
+
+            # # Run the command
+            result_move = subprocess.run(cmd_move, cwd= Path("ia_model/darknet"),capture_output=True, text=True)
+            print(result_move.stdout)
+
+            results.append({
+                "processed_filename": image_processed_name,
+                "stdout": result_move.stdout,
+                "stderr": result_move.stderr
+            })
             # # Test case
             # ls = ["ls", "-al"]
             # result = subprocess.run(ls, cwd= Path("app/routes/darknet"),capture_output=True, text=True)
