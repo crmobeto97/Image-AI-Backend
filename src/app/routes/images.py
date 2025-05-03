0d10eb68aa1e6e959a7c16d7e820f0ff0e683f81
Beto Ocampo <crmobeto97@gmail.com>
2025-05-03 00:31:16 -0600

    add get file

--- src/app/routes/images.py
+++ src/app/routes/images.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, File, UploadFile, HTTPException, Query, Form
+from fastapi.responses import FileResponse
 import os
 from pathlib import Path
 from typing import List
@@ -88,16 +89,35 @@ async def upload_file(token: str = Form(...), files: List[UploadFile] = File(...
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al guardar archivo: {str(e)}")
 
+@router.get("/file")
+async def get_image_file(uuid: str = Query(..., title="UUID de la carpeta"), status_process: bool = Query(..., title="false = raw, true = processed") , filename: str = Query(..., title="nombre del archivo")):
+    try:
+        folder = RAW_FOLDER
+        if status_process:
+            folder = PROCESSED_FOLDER
+        
+        file_path = TEMP_DIR / uuid / folder / filename
 
+        if not file_path.exists() or not file_path.is_file():
+            raise HTTPException(status_code=404, detail="Archivo no encontrado")
+
+        return FileResponse(
+            path=file_path,
+            media_type="image/jpeg",
+            filename=filename
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al obtener archivo: {str(e)}")
 
 @router.get("/processed")
 async def check_folder(uuid: str = Query(..., title="UUID de la carpeta")):
     try:
         folder_path = TEMP_DIR / uuid / PROCESSED_FOLDER 
  
-        # if not folder_path.exists():
-        #     folder_path.mkdir(parents=True, exist_ok=True)
-        #     return {"message": "Carpeta creada", "files": []}
+        if not folder_path.exists():
+            folder_path.mkdir(parents=True, exist_ok=True)
+            return {"message": "Carpeta creada", "files": []}
 
         image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"}
         image_files = [
